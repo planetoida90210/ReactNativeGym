94dfc525f2fd121cd9a90e96798a7caa9c2901f6
Michał Bryk <galeriakoloru90210@gmail.com>
2024-01-07 15:01:35 +0100

    initialize rn+tailwind old version (3.3.2) with nativewind, waiting for v4 is stable

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Index() {
+  return (
+    <View className='bg-red-300'>
+      <Text>1st screen</Text>
+    </View>
+  )
+}
